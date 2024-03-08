f24d1fd1cc9b88dbc2292553a044cf83972a5f7a
c.raia <calogeroraia3@gmail.com>
2024-03-08 12:34:48 +0100

    compito

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useContext } from 'react';
+import axios from 'axios';
+import { TodoContext } from './context/TodoContext'; 
+import Todo from './Todo';
+
+
+const App = () => {
+  const { todos, setTodos } = useContext(TodoContext);
+    useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setTodos(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <ul>
+        {todos.map(todo => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/TodoProvider.js
@@ -0,0 +1,9 @@
+const TodoProvider = ({ children }) => {
+    const [todos, setTodos] = useState([]);
+  
+    return (
+      <TodoContext.Provider value={{ todos, setTodos }}>
+        {children}
+      </TodoContext.Provider>
+    );
+  };
